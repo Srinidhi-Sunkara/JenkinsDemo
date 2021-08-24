052bac4e18736774a5325a3cb36371aca86d0ecd
SrinidhiSunkara2000 <srinidhisunkara@outlook.com>
2021-08-24 15:23:39 +0530

    second commit

--- SpringBootGitJenkinsDemo/src/main/java/com/example/demo/SpringBootGitJenkinsDemoApplication.java
+++ SpringBootGitJenkinsDemo/src/main/java/com/example/demo/SpringBootGitJenkinsDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootGitJenkinsDemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome");
 		SpringApplication.run(SpringBootGitJenkinsDemoApplication.class, args);
 	}
 
